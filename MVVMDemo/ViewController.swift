7c81d9cb71df854f40583ff8a7ec075e7d5209a2
Neb <nex.shaw.jobs@gmail.com>
2021-05-30 14:41:58 -0500

    _

--- MVVMDemo/ViewController.swift
+++ MVVMDemo/ViewController.swift
@@ -19,57 +19,26 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         viewModel = mVVMDemoViewModel
-        titleValue.text = viewModel?.headerTitle
-        position1.text = viewModel?.positionTitle
-        self.getData()
-    }
-    // work on dynamic binding 
-    func getData(){
-        var str = "{\"header\":{\"title\":\"This is your header title\"},\"details\":[{\"title\":\"1. This is your first subtitle.\",\"position\":\"1\"},{\"title\":\"2. This is your second subtitle.\",\"position\":\"2\"},{\"title\":\"3. This is your third subtitle\",\"position\":\"3\"}]}"
-        let data = Data(str.utf8)
-
-        do {
-            // make sure this JSON is in the format we expect
-            if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
-                // try to read out a string array
-                if let positions = json["details"] as? [Dictionary<String, Any>] {
-                    print(json)
-                    for position in positions {
-                        print(position)
-                    }
-                }
-                if let headers = json["header"] as? Dictionary<String, Any>{
-                    print (headers)
-                    for header in headers {
-                        print (header)
-                    }
-                }
-            }
-        } catch let error as NSError {
-            print("Failed to load: \(error.localizedDescription)")
-        }
+        titleValue.text = viewModel?.getHeaderTitle
+        position1.text = viewModel?.getPositions[0].title
+        //self.getData()
     }
 }
-
 //ViewModel
-var mVVMDemoViewModel: MVVMDemoViewModel = MVVMDemoViewModel(position: p, header: h)
-
+var mVVMDemoViewModel: MVVMDemoViewModel = MVVMDemoViewModel(positions: ps, header: h)
 class MVVMDemoViewModel {
     
-    private let position:Position
+    private let positions:[Position]
     private let header: Header
     
-    init (position: Position, header: Header){
-        self.position = position
+    init (positions: [Position], header: Header){
+        self.positions = positions
         self.header = header
     }
-    public var positionTitle: String {
-        return position.title
+    public var getPositions: [Position] {
+        return positions
     }
-    public var positionPosition: Int {
-        return position.position
-    }
-    public var headerTitle: String {
+    public var getHeaderTitle: String {
         return header.title
     }
 
@@ -77,17 +46,55 @@ class MVVMDemoViewModel {
 //End view model
 
 //Model
+
+public class Model {
+    
+}
+
 struct Header {
     var title: String
 }
 struct details {
     var positins:[Position]
+    var header:Header
 }
 struct Position {
     var position: Int
     var title: String
 }
 
-let p:Position = Position(position: 1, title: "title1")
-let h:Header = Header(title: "header title")
+var ps:[Position] = []
+let h:Header = Header(title: "")
+
+
+//use codeble?
+func getData(){
+    var str = "{\"header\":{\"title\":\"This is your header title\"},\"details\":[{\"title\":\"1. This is your first subtitle.\",\"position\":\"1\"},{\"title\":\"2. This is your second subtitle.\",\"position\":\"2\"},{\"title\":\"3. This is your third subtitle\",\"position\":\"3\"}]}"
+    let data = Data(str.utf8)
+    do {
+        // make sure this JSON is in the format we expect
+        if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+            // try to read out a string array
+            if let positions = json["details"] as? [Dictionary<String, Any>] {
+                print(json)
+                for position in positions {
+                    print(position)
+                    var p:Position = Position(position: 0, title: "")
+                    p.position = position["position"] as! Int
+                    p.title = position["title"] as! String
+                    ps.append(p)
+                }
+            }
+            if let headers = json["header"] as? Dictionary<String, Any>{
+                print (headers)
+                //h = Header(title: headers[0].title)
+                for header in headers {
+                    print (header)
+                }
+            }
+        }
+    } catch let error as NSError {
+        print("Failed to load: \(error.localizedDescription)")
+    }
+}
 //End Model
